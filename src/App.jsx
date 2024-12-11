4a7920203734a2b19665bb1f42802cf621b1ec9e
cmolinamun <125497252+cmolinamun@users.noreply.github.com>
2024-12-10 23:43:53 -0300

    Add Cart component

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,16 @@ import Home from './components/Home'
 import Footer from './components/Footer'
 import Register from './components/Register'
 import Login from './components/Login'
+import Cart from './components/Cart'
 
 function App() {
   return (
     <>
       <Navbar />
-      {/* <Home /> */}
-      <Register />
-      <Login />
+      {/*<Home /> */}
+      {/* <Register /> */}
+      {/* <Login /> */}
+      <Cart />
       <Footer />
     </>
   )
